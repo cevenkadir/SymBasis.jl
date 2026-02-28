884c53386d6eca47bb3c8ef0a803a896f29c8693
Kadir Çeven <kadir.ceven@uni-goettingen.de>
2026-02-28 22:23:08 +0100

    Update exports in SymGroups module to include check_multipole and SpinMultipole

--- src/symgroups/SymGroups.jl
+++ src/symgroups/SymGroups.jl
@@ -6,10 +6,10 @@ export _make_hashset
 
 include("predefined_funcs.jl")
 export AbstractSymSpec
-export TotalMagnetization, SpinInversion
+export TotalMagnetization, SpinMultipole, SpinInversion
 export Translational, SpatialReflection, Rotational
-export check_Nₛ, check_perm, check_flip
-export apply_Nₛ, apply_perm, apply_flip
+export check_Nₛ, check_perm, check_flip, check_multipole
+export apply_Nₛ, apply_perm, apply_flip, apply_multipole
 export sym
 
 end
